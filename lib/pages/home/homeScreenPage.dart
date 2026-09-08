48d331044cd2c29316756141043eca84f5abb08b
shivani <shivani@greensoftsolutions.co.in>
2026-09-08 11:21:13 +0530

    add the keystore and  keystoreproperty  and  build setup

--- lib/pages/home/homeScreenPage.dart
+++ lib/pages/home/homeScreenPage.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math' as math;
 
-import 'package:avatar_glow/avatar_glow.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:flutter_svg/svg.dart';
